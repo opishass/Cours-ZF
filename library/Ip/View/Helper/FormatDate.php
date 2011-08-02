683b552d2b0cb768801ad529b2a4366f2bde371b
frahamefy <randriaf@wanadoo.fr>
2011-08-02 14:18:31 +0200

    implantation library

---
+++ library/Ip/View/Helper/FormatDate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Abstract class for Zend_View to help enforce private constructs.
+ *
+ * @category   Ip
+ * @package    Ip_View
+ * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+class Ip_View_Helper_FormatDate extends Zend_View_Helper_Abstract 
+{
+	
+	public function formatDate() {
+		
+		return 'DATE FORMATER SSSSS';
+		
+	}
+	
+	
+}
